3ae34abb1c3b959b4979e05da959f1b2b4337d26
fel255 <feliciandehecker@gmail.com>
2025-06-23 21:55:20 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -24,9 +24,9 @@ const config = {
   MODE: process.env.MODE || "public",
   BOT_NAME: process.env.BOT_NAME || "RAHEEM-XMD-2",
   MENU_IMAGE: process.env.MENU_IMAGE || "https://files.catbox.moe/vgb4cw.jpg",
-  DESCRIPTION: process.env.DESCRIPTION || "WELCOME TO RAHEEM-XMD-2",
-  OWNER_NAME: process.env.OWNER_NAME || "RAHEEM-CM",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "255763111390",
+  DESCRIPTION: process.env.DESCRIPTION || "WELCOME TO TECH BOT",
+  OWNER_NAME: process.env.OWNER_NAME || "SKIDDBMX",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "255741927849",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
